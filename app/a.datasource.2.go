fe67a0154371a7f01ba6ebddc847ab3f2537ad1a
Ronald Andres <ronald@insky-inc.com>
2018-01-30 17:27:47 +0800

    removed contacts entry

--- app/a.datasource.2.go
+++ app/a.datasource.2.go
@@ -116,38 +116,12 @@ func init() {
 		cmd.NewParameter("visit_id", "int", "in", 0, 0)
 	})
 
-	dbase.Connections["DBApp"].NewCommand("GetContacts", "GetContacts", "procedure", func(cmd dbase.ICommand) {
-		cmd.NewParameter("id", "int", "in", 0, 0)
-		cmd.NewParameter("name_id", "int", "in", 0, 0)
-		cmd.NewParameter("sort", "string", "in", 200, "")
-		cmd.NewParameter("order", "string", "in", 10, "")
-		cmd.NewParameter("visit_id", "int", "in", 0, 0)
-	})
-
 	dbase.Connections["DBApp"].NewCommand("GetBanks", "GetBanks", "procedure", func(cmd dbase.ICommand) {
 		cmd.NewParameter("id", "int", "in", 0, 0)
 		cmd.NewParameter("name_id", "int", "in", 0, 0)
 		cmd.NewParameter("sort", "string", "in", 200, "")
 		cmd.NewParameter("order", "string", "in", 10, "")
 		cmd.NewParameter("visit_id", "int", "in", 0, 0)
-	})
-
-	dbase.Connections["DBApp"].NewCommand("AddContacts", "AddContacts", "procedure", func(cmd dbase.ICommand) {
-		cmd.NewParameter("id", "int", "inout", 0, 0)
-		cmd.NewParameter("name_id", "int", "in", 0, 0)
-		cmd.NewParameter("title", "string", "in", 20, "")
-		cmd.NewParameter("full_name", "string", "in", 50, "")
-		cmd.NewParameter("department", "string", "in", 50, "")
-		cmd.NewParameter("position", "string", "in", 50, "")
-		cmd.NewParameter("phone", "string", "in", 20, "")
-		cmd.NewParameter("mobile", "string", "in", 20, "")
-		cmd.NewParameter("fax", "string", "in", 20, "")
-		cmd.NewParameter("email", "string", "in", 60, "")
-
-		cmd.NewParameter("action", "int", "in", 0, 10)
-		cmd.NewParameter("visit_id", "int", "in", 0, 0)
-		cmd.NewParameter("action_status_id", "int", "inout", 0, 0)
-		cmd.NewParameter("action_msg", "string", "inout", 200, "")
 	})
 
 	dbase.Connections["DBApp"].NewCommand("AddBanks", "AddBanks", "procedure", func(cmd dbase.ICommand) {
